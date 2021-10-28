89d61f836af5c608835698fed67834eea70c21c4
ThiagoPiresBarros <89088630+ThiagoPiresBarros@users.noreply.github.com>
2021-10-28 18:52:08 -0300

    upload completo

--- README.md
+++
@@ -1,2 +0,0 @@
-"# cadastro-pam" 
-"# projeto-cadastro-pam" 
--- www/js/firebase.js
+++ www/js/firebase.js
@@ -1,13 +1,17 @@
+
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "AIzaSyCeBFU85K-gcT_MXZbMxGgspHpPsQM5iJ8",
-    authDomain: "cadastro-pam.firebaseapp.com",
-    projectId: "cadastro-pam",
-    storageBucket: "cadastro-pam.appspot.com",
-    messagingSenderId: "147729738033",
-    appId: "1:147729738033:web:e14bcce620129a1196e810",
-    measurementId: "G-2Z0CLCFDN6"
-  };
+  apiKey: "AIzaSyCeBFU85K-gcT_MXZbMxGgspHpPsQM5iJ8",
+  authDomain: "cadastro-pam.firebaseapp.com",
+  projectId: "cadastro-pam",
+  storageBucket: "cadastro-pam.appspot.com",
+  messagingSenderId: "147729738033",
+  appId: "1:147729738033:web:e14bcce620129a1196e810",
+  measurementId: "G-2Z0CLCFDN6"
+};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
